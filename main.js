45fdc7ec74ddaad8aa875af81b684b9bcc1a6b6a
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-08-13 11:36:03 +0700

    main.js

--- main.js
+++ main.js
@@ -10,12 +10,29 @@ var value = document.querySelector('.value');
 var time = document.querySelector('.time');
 var content = document.querySelector('.content');
 
-async function changweatherUI(){
-    search.value.trim()
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=c523697cf97add0c2cf3f42505863f26`
+async function changeWeatherUI(){
+  let capitalSearch = search.value.trim()
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=c523697cf97add0c2cf3f42505863f26`
 
     let data = await fetch(apiURL).then(res => res.json())
-    console.log(data)
+    if(data.cod == 200){
+      content.classList.remove('hide')
+      city.innerText = data.name
+      country.innerText = data.sys.country
+      visibility.innerText = data.visibility + 'm'
+      wind.innerText = data.wind.speed + 'm/s'
+      sun.innerText = data.main.humidity + '%'
+      value.innerText = (data.main.temp -273.15)
+      shortDesc.innerText = data.weather[0] ? data.weather[0].main : ''
+      time.innerText = new Date().toLocaleString('vi')
+    }else{
+      content.classList.add('hide')
+    }
 }
 
-changeweatherUI()
+
+search.addEventListener('keypress', function(e){
+  if(e.code === 'Enter'){
+    changeWeatherUI()
+  }
+})
