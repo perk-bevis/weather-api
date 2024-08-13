647f286f28d8c2a48c4ba97cc5a8e6281032fdb4
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-08-13 09:33:56 +0700

    main.js

--- main.js
+++ main.js
@@ -11,27 +11,11 @@ var time = document.querySelector('.time');
 var content = document.querySelector('.content');
 
 async function changweatherUI(){
-    let capitalSearch = search.value.trim()
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=c523697cf97add0c2cf3f42505863f26`
+    search.value.trim()
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=c523697cf97add0c2cf3f42505863f26`
 
     let data = await fetch(apiURL).then(res => res.json())
-    if(data.cod == 200){
-       content.classList.remove('hide')
-       city.innerText = data.name
-       country.innerText = data.sys.country
-       visibility.innerText = data.visibility + 'm'
-       wind.innerText = data.wind.speed + 'm/s'
-       sun.innerText = data.main.humidity + '%'
-       value.innerText = Math.round((data.main.temp -273.15))
-       shortDesc.innerText = data.weather[0] ? data.weather[0].main : ''
-       time.innerText = new Date().toLocaleString('vi')
-    }else{
-       content.classList.add('hide')
-    }
+    console.log(data)
 }
 
-search.addEventListener('keypress', function(e){
-  if(e.code === 'Enter'){
-    changeweatherUI()
-  }
-})
+changeweatherUI()
