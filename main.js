4186eebc968ca85f93f6ebf058bbb84806f69f91
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-08-13 16:28:46 +0700

    main.js

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ var sun = document.querySelector('.sun span');
 var value = document.querySelector('.value');
 var time = document.querySelector('.time');
 var content = document.querySelector('.content');
+var body = document.querySelector('.body');
 
-async function changeWeatherUI(){
-  let capitalSearch = search.value.trim()
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=c523697cf97add0c2cf3f42505863f26`
+async function changeWeatherUI(capitalSearch){
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${encordeURIComponent(capitalSearch)}&appid=c523697cf97add0c2cf3f42505863f26`
 
     let data = await fetch(apiURL).then(res => res.json())
     if(data.cod == 200){
@@ -22,17 +22,36 @@ async function changeWeatherUI(){
       visibility.innerText = data.visibility + 'm'
       wind.innerText = data.wind.speed + 'm/s'
       sun.innerText = data.main.humidity + '%'
-      value.innerText = (data.main.temp -273.15)
+      let temp = (data.main.temp -273.15).toFixed(2) + '°C'
+      value.innerText = temp
       shortDesc.innerText = data.weather[0] ? data.weather[0].main : ''
       time.innerText = new Date().toLocaleString('vi')
+      
+      body.setAttribute('class', 'hot')
+      
+      if(temp <= 25) {
+        body.setAttribute('class', 'cool')
+      }
+      
+      if(temp <= 22) {
+        body.setAttribute('class', 'warm')
+      }
+      
+      if(temp <= 19) {
+        body.setAttribute('class', 'cold')
+      }
+      
     }else{
       content.classList.add('hide')
     }
 }
 
 
-search.addEventListener('keypress', function(e){
-  if(e.code === 'Enter'){
-    changeWeatherUI()
+search.addEventListener('input', function(e){
+  if(e.key === 'Enter'){
+    let capitalSearch = search.value.trim()
+    changeWeatherUI(capitalSearch)
   }
 })
+
+changeWeatherUI('ha noi')
